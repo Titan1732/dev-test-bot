745c560d20a1b0abf493c20b74ca0bc89136b3f1
Titan1732 <1524912+Titan1732@users.noreply.github.com>
2024-04-14 15:59:32 -0400

    Test Commit

--- rsvp_confirmation.py
+++ rsvp_confirmation.py
@@ -3,7 +3,7 @@ import discord
 class infoEmbed(discord.Embed):
     def __init__(self):
 
-        super().__init__(title="Flex Friday 💪", type="rich")
+        super().__init__(title="Info Embed", type="rich", description="Put explanation of how to use here, maybe put event info too")
 
 
 class Confirm(discord.ui.View):
@@ -11,18 +11,16 @@ class Confirm(discord.ui.View):
         super().__init__()
         self.value = None
 
-    # When the confirm button is pressed, set the inner value to `True` and
-    # stop the View from listening to more input.
-    # We also send the user an ephemeral message that we're confirming their choice.
+
     @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
     async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Thanks for confirming your attendance to Flex Friday!')
+        await interaction.response.send_message('Thanks for confirming your attendance to EVENT_TITLE!')
         self.value = True
         self.stop()
 
     # This one is similar to the confirmation button except sets the inner value to `False`
     @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
     async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Removing ')
+        await interaction.response.send_message(f'Cancelling RSVP for {interaction.user.global_name}')
         self.value = False
         self.stop()
